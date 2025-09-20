fdf02f2f6038f6d0de5429581f1a46742d0cf33c
Евгений <53685733+SiberianJohnny@users.noreply.github.com>
2025-09-20 15:04:42 +0700

    Location permission request

--- App.tsx
+++ App.tsx
@@ -1,9 +1,32 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
+import { PermissionsAndroid, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation/RootNavigation';
+import { useEffect } from 'react';
 
 function App() {
+  const requestGeolocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Geolocation Permission',
+          message: 'This App needs access to your geolocation.',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      return granted === PermissionsAndroid.RESULTS.GRANTED;
+    } catch (err) {
+      console.warn(err);
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    requestGeolocationPermission();
+  }, []);
+
   return (
     <NavigationContainer>
       <SafeAreaProvider style={styles.container}>
