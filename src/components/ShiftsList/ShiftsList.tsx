81a5bf9b6f5ab8bf05ea3048f6625b146d7762bb
Евгений <53685733+SiberianJohnny@users.noreply.github.com>
2025-09-24 18:06:51 +0700

    Add shifts list component

---
+++ src/components/ShiftsList/ShiftsList.tsx
@@ -0,0 +1,68 @@
+import {
+  View,
+  Text,
+  FlatList,
+  ListRenderItem,
+  StyleSheet,
+  Image,
+} from 'react-native';
+import React from 'react';
+import { IGetShifts, IShift } from '../../types/responses';
+
+interface IProps {
+  listData: IGetShifts | undefined;
+}
+
+const ShiftsList = ({ listData }: IProps) => {
+  const renderItem: ListRenderItem<IShift> = ({ item }) => {
+    return (
+      <View style={styles.shiftItem}>
+        <View style={styles.shiftItemInfo}>
+          <Text style={styles.shiftItemTitle}>{item.companyName}</Text>
+          <Image
+            source={{ uri: item.logo }}
+            style={{ width: 32, height: 32, borderRadius: 8 }}
+          />
+        </View>
+        <View style={styles.shiftItemInfo}>
+          <Text>Требуется чел.: {item.planWorkers}</Text>
+          <Text>Сейчас чел.: {item.currentWorkers}</Text>
+          <Text>Стоимость: {item.priceWorker}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  if (!listData) return <Text>Нет данных о сменах</Text>;
+
+  return (
+    <FlatList
+      contentContainerStyle={{
+        paddingVertical: 8,
+        rowGap: 12,
+      }}
+      data={listData?.data}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  shiftItem: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+    borderRadius: 8,
+    padding: 8,
+    gap: 8,
+  },
+  shiftItemTitle: {},
+  shiftItemInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: 4,
+  },
+});
+
+export default ShiftsList;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,26 @@
 import { useEffect, useState } from 'react';
-import {
-  View,
-  Text,
-  PermissionsAndroid,
-  TouchableOpacity,
-  StyleSheet,
-  Linking,
-} from 'react-native';
-import Geolocation from '@react-native-community/geolocation';
+import { View, PermissionsAndroid, StyleSheet } from 'react-native';
+import Geolocation, {
+  GeolocationResponse,
+} from '@react-native-community/geolocation';
 import WarningBlock from '../components/WarningBlock/WarningBlock';
+import { useGetShiftsQuery } from '../../store/api/shiftsApi';
+import ShiftsList from '../components/ShiftsList/ShiftsList';
 
 const HomeScreen = () => {
   const [locationPermission, setLocationPermission] = useState<boolean>(false);
+  const [currentPosition, setCurrentPosition] = useState<GeolocationResponse>();
 
-  Geolocation.getCurrentPosition(info => console.log(info));
+  const {
+    data: shiftsData,
+    isLoading: isShiftsDataLoading,
+    isFetching: isShiftsDataFetching,
+  } = useGetShiftsQuery({
+    latitude: currentPosition?.coords.latitude.toString(),
+    longitude: currentPosition?.coords.longitude.toString(),
+  });
+
+  console.log(currentPosition, shiftsData);
 
   const requestGeolocationPermission = async () => {
     try {
@@ -36,11 +43,16 @@ const HomeScreen = () => {
 
   useEffect(() => {
     requestGeolocationPermission();
+    Geolocation.getCurrentPosition(info => setCurrentPosition(info));
   }, []);
 
   return (
     <View style={styles.container}>
-      {!locationPermission ? <WarningBlock /> : <Text>Home Screen</Text>}
+      {!locationPermission ? (
+        <WarningBlock />
+      ) : (
+        <ShiftsList listData={shiftsData} />
+      )}
     </View>
   );
 };
