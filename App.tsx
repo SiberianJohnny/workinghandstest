e596b030759bcdf889a017374bebf16cb402ddb0
Евгений <53685733+SiberianJohnny@users.noreply.github.com>
2025-09-24 17:14:11 +0700

    Add an error when permissions is not granted

--- App.tsx
+++ App.tsx
@@ -1,34 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { PermissionsAndroid, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation/RootNavigation';
-import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
 function App() {
-  const requestGeolocationPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: 'Geolocation Permission',
-          message: 'This App needs access to your geolocation.',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      return granted === PermissionsAndroid.RESULTS.GRANTED;
-    } catch (err) {
-      console.warn(err);
-      return false;
-    }
-  };
-
-  useEffect(() => {
-    requestGeolocationPermission();
-  }, []);
-
   return (
     <Provider store={store}>
       <NavigationContainer>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,22 +1,91 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  PermissionsAndroid,
+  TouchableOpacity,
+  StyleSheet,
+  Linking,
+} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 
 const HomeScreen = () => {
+  const [locationPermission, setLocationPermission] = useState<boolean>(false);
+
   Geolocation.getCurrentPosition(info => console.log(info));
 
+  const requestGeolocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Geolocation Permission',
+          message: 'This App needs access to your geolocation.',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+
+      setLocationPermission(granted === PermissionsAndroid.RESULTS.GRANTED);
+    } catch (err) {
+      console.warn(err);
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    requestGeolocationPermission();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'lightgray',
-      }}
-    >
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      {!locationPermission ? (
+        <View style={styles.warningBlock}>
+          <Text style={styles.warningText}>
+            Для работы приложения необходимо предоставить доступ к геолокации
+          </Text>
+          <TouchableOpacity
+            style={styles.requestPermissionButton}
+            onPress={Linking.openSettings}
+          >
+            <Text style={styles.requestPermissionButtonText}>
+              Предоставить доступ
+            </Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <Text>Home Screen</Text>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  warningBlock: {
+    alignItems: 'center',
+    backgroundColor: 'lightgray',
+    borderRadius: 8,
+    padding: 8,
+  },
+  warningText: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  requestPermissionButton: {
+    backgroundColor: '#000',
+    padding: 8,
+    borderRadius: 4,
+    marginTop: 8,
+  },
+  requestPermissionButtonText: {
+    color: '#FFF',
+  },
+});
 
 export default HomeScreen;
